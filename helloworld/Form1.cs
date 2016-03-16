50573e66b0d50612bc56745218d5b94131e19925
Kyaw K. Aung <vannymore@yahoo.com>
2016-03-16 10:42:27 -0400

    second commit addded form load event.

--- helloworld/Form1.cs
+++ helloworld/Form1.cs
@@ -16,5 +16,10 @@ namespace helloworld
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this is comment
+        }
     }
 }
